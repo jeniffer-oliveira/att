c59ff60fe067464f47d2c221403f711a24f62f2f
jeniffer-oliveira <100139970+jeniffer-oliveira@users.noreply.github.com>
2022-09-16 22:34:10 -0300

    1 file modified, 2 added

---
+++ src/app/components/jeniffer-simple-interests/jeniffer-simple-interests.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jeniffer-simple-interests',
+  templateUrl: './jeniffer-simple-interests.component.html',
+  styleUrls: ['./jeniffer-simple-interests.component.css']
+})
+export class JenifferSimpleInterestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
